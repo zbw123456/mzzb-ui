384b19662fd246f95e8b3a08027ea72c3def48db
fuhaiwei <fuhaiwei@126.com>
2018-12-16 16:59:45 +0800

    feat(project): add antd, use layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import './App.scss';
 import { Switch, Route, Link } from 'react-router-dom';
 import Sakuras from './components/Sakuras';
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/sakuras">Sakuras</Link>
-      </header>
-      <Switch>
-        <Route path="/" exact={true}>
-          <div className="text-content">Hello, This is home page.</div>
-        </Route>
-        <Route path="/sakuras" component={Sakuras} />
-        <Route path="*">
-          {({ location }) => (
-            <div className="text-content">404 Not Found: {location.pathname}</div>
-          )}
-        </Route>
-      </Switch>
+      <Layout>
+        <Header>
+          <Link to="/">Home</Link>
+          <Link to="/sakuras">Sakuras</Link>
+        </Header>
+        <Content>
+          <Switch>
+            <Route path="/" exact={true}>
+              <div className="text-content">Hello, This is home page.</div>
+            </Route>
+            <Route path="/sakuras" component={Sakuras} />
+            <Route path="*">
+              {({ location }) => (
+                <div className="text-content">404 Not Found: {location.pathname}</div>
+              )}
+            </Route>
+          </Switch>
+        </Content>
+        <Footer>
+          Copyright 2018 mingzuozhibi.com
+        </Footer>
+      </Layout>
     </div>
   )
 }
