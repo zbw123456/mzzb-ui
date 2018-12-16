1da0e811861e6857c051c361aab8217a3621f10f
fuhaiwei <fuhaiwei@126.com>
2018-12-16 22:17:21 +0800

    feat(project): cleanup and rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.scss';
 
 const rootNode = (
   <BrowserRouter>
@@ -12,8 +11,3 @@ const rootNode = (
 );
 
 ReactDOM.render(rootNode, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
