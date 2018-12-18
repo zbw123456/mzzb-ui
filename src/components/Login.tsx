1b80c1877172c49f1ac9bf017ea529abd652a69d
fuhaiwei <fuhaiwei@126.com>
2018-12-19 03:19:06 +0800

    feat(Login.tsx): add login button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import Icon from 'antd/lib/icon';
 import Input from 'antd/lib/input';
-import { useDocumentTitle, useInput } from '../hooks';
+import Alert from 'antd/lib/alert';
+import Button from 'antd/lib/button';
+import { useDocumentTitle, useInput, useGetJson } from '../hooks';
+
+interface ISession {
+  isLogged: boolean
+  userName: string
+  onlineUserCount: number
+  userRoles: string[]
+}
 
 function Login() {
   useDocumentTitle('Login')
   const username = useInput('admin')
   const password = useInput('123456')
+  const session = useGetJson<ISession>('/api/session')
+  if (!session.data) {
+    return (
+      <div className="Login">
+        {session.error && (
+          <Alert type="error" message={session.error} />
+        )}
+      </div>
+    )
+  }
+  const { isLogged } = session.data
+  if (isLogged) {
+    return (
+      <div className="Login">
+        {session.data.userName} is logged.
+      </div>
+    )
+  }
   return (
     <div className="Login">
       <div className='ant-input-form'>
@@ -21,6 +48,7 @@ function Login() {
           prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
           placeholder="Enter password"
         />
+        <Button type="primary">Login</Button>
       </div>
     </div>
   )
