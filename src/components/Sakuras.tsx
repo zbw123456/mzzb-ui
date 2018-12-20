ebac22128443abec0bc6c5dcdf5cb35133c803c5
fuhaiwei <fuhaiwei@126.com>
2018-12-20 20:21:52 +0800

    style(project): reset table css

--- src/components/Sakuras.tsx
+++ src/components/Sakuras.tsx
@@ -4,7 +4,6 @@ import Table, { ICol } from '../libraries/Table';
 import DataWarpper from '../libraries/DataWarpper';
 import { formatTimeout } from '../functions/format'
 import { useDocumentTitle, useGetJson } from '../hooks';
-import './Sakuras.scss';
 
 interface IRow {
   id: number
